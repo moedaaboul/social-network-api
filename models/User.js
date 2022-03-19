40b584d40d0e92ddef06d04bd0bb22586d858158
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-03-19 10:46:58 +0000

    add friendCount virtual

--- models/User.js
+++ models/User.js
@@ -1,31 +1,43 @@
 const mongoose = require('mongoose');
 
 // More on validation here: https://mongoosejs.com/docs/validation.html
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, 'Please provide name'],
-    unique: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Please provide name'],
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide email'],
+      match: [
+        /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        'Please provide valid email',
+      ],
+      unique: true,
+    },
+    thoughts: {
+      type: Array,
+      default: [],
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
   },
-  email: {
-    type: String,
-    required: [true, 'Please provide email'],
-    match: [
-      /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
-      'Please provide valid email',
-    ],
-    unique: true,
-  },
-  thoughts: {
-    type: Array,
-    default: [],
-  },
-  friends: {
-    type: Array,
-    default: [],
-  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
 });
 
 // Set up the model, which is a representation for the collection
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);

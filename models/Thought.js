63ceed7e319343eca3ce24ff92b403860688d82d
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-03-19 21:57:07 +0000

    adds a getter method to format createdAt date in thoughtSchema

--- models/Thought.js
+++ models/Thought.js
@@ -12,6 +12,12 @@ const thoughtSchema = new mongoose.Schema(
     createdAt: {
       type: Date,
       default: Date.now(),
+      get: (createdAt) =>
+        Intl.DateTimeFormat('en-US', {
+          month: 'long',
+          day: 'numeric',
+          year: 'numeric',
+        }).format(new Date(createdAt)),
     },
     username: {
       type: String,
@@ -25,10 +31,19 @@ const thoughtSchema = new mongoose.Schema(
   {
     toJSON: {
       virtuals: true,
+      // need to set getters is "true" due to the get method on createdAt. This doesn't affect the db, but the json output on query
+      getters: true,
     },
     id: false,
   }
 );
+
+// const formatDate = (createdAt) =>
+//   Intl.DateTimeFormat('en-US', {
+//     month: 'long',
+//     day: 'numeric',
+//     year: 'numeric',
+//   }).format(new Date(createdAt));
 
 thoughtSchema.virtual('reactionCount').get(function () {
   return this.reactions.length;
